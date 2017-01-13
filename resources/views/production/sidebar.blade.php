74f2a3b06f20fa62d98110701c4c455af71852bf
林于斌 <papago75@gmail.com.tw>
2017-01-13 09:03:25 +0800

    修正管理功能判斷

--- resources/views/production/sidebar.blade.php
+++ resources/views/production/sidebar.blade.php
@@ -1,7 +1,16 @@
 <div class="list-group table-of-content">
     <a class="list-group-item" href="{{ url('/production/today') }}">當日生產線一覽</a>
     <a class="list-group-item" href="{{ url('/production/info') }}">瓶號生產資料庫</a>
-    @if (request()->ip() == '192.168.10.1')
+    @php
+        $set = ['192.168.10.1', '192.168.168.41', '192.168.168.50'];
+        $auth = false;
+        foreach ($set as $s) {
+            if (request()->ip() == $s) {
+                $auth = true;
+            }
+        }
+    @endphp
+    @if ($auth)
         <a class="list-group-item" href="{{ url('/production/importToday') }}">當日生產線資料管理</a>
         <a class="list-group-item" href="{{ url('/production/importGlass') }}">瓶號生產資料庫管理</a>
     @endif
